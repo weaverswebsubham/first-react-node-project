6adac0c50b5dc2993388fa0ea8bf44827e5484c3
weaverswebsubham <subham.das@weavers-web.com>
2022-09-11 21:40:09 +0530

    sign up page.

--- frontend/src/Components/Pages/SignupPageComponent.js
+++ frontend/src/Components/Pages/SignupPageComponent.js
@@ -5,28 +5,26 @@ import TextFieldComponent from "../UI-Components/TextFieldComponent";
 function LoginPageComponent(props) {
   return (
     <>
-      <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form action="">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
-          </h5>
+      <div className="flex flex-row min-h-screen justify-center items-center">
+        <form
+          className="w-full max-w-md bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 ring-4"
+          action=""
+        >
+          <div className="text-center mb-5">
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+              Sign up
+            </h5>
+          </div>
           <div>
             <TextFieldComponent type="text" placeholder="Name" />
             <TextFieldComponent type="text" placeholder="Email" />
-            <TextFieldComponent
-              type="text"
-              placeholder="Phone Number"
-            />
-            <TextFieldComponent
-              type="text"
-              placeholder="Password"
-            />
-            <TextFieldComponent
-              type="text"
-              placeholder="Confirm Password"
-            />
+            <TextFieldComponent type="text" placeholder="Phone Number" />
+            <TextFieldComponent type="text" placeholder="Password" />
+            <TextFieldComponent type="text" placeholder="Confirm Password" />
           </div>
-          <SubmitButtonComponent type="submit" name="Submit" />
+          <div className="text-center mt-6">
+            <SubmitButtonComponent type="submit" name="Submit" />
+          </div>
         </form>
       </div>
     </>
--- frontend/src/Components/UI-Components/SubmitButtonComponent.js
+++ frontend/src/Components/UI-Components/SubmitButtonComponent.js
@@ -4,7 +4,7 @@ function SubmitButtonComponent(props) {
   return (
     <>
       <div>
-        <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type={props.type}>{props.name}</button>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type={props.type}>{props.name}</button>
       </div>
     </>
   );
